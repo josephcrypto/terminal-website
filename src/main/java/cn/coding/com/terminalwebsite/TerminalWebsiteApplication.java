8f745266ae9c293729b67dbfce053bbb466138b7
joseph <justgivemegame@gmail.com>
2021-10-16 22:07:48 +0630

    Initial first commit

---
+++ src/main/java/cn/coding/com/terminalwebsite/TerminalWebsiteApplication.java
@@ -0,0 +1,13 @@
+package cn.coding.com.terminalwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerminalWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/coding/com/terminalwebsite/TerminalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.coding.com.terminalwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
